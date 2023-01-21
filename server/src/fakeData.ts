096ef2f5ab77e871e970af765205a8122bbb97f3
christianpinna <christian.a.pinna@gmail.com>
2023-01-21 14:11:21 +0000

    Add fakeData.ts

---
+++ server/src/fakeData.ts
@@ -0,0 +1,13 @@
+const duneImperium = {
+  name: "Dune: Imperium",
+  libraryLocation: "19c",
+  bestGroupSize: 3,
+  gameTimeInSeconds: 12600,
+  description: "deck builder",
+  imageLink: "https://cf.geekdo-images.com/PhjygpWSo-0labGrPBMyyg__imagepage/img/BjM3LyahJ4IQ2ov5MkzkHatbmUc=/fit-in/900x600/filters:no_upscale():strip_icc()/pic5666597.jpg",
+  BGGLink: "https://boardgamegeek.com/boardgame/316554/dune-imperium"
+};
+
+export const dummyGamesData = {
+  duneImperium,
+};
