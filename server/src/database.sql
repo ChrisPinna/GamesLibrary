4c787e5fa822fea6f272031b86e351bcbc6d9261
christianpinna <christian.a.pinna@gmail.com>
2023-01-21 14:10:51 +0000

    Add database.sql

---
+++ server/src/database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE games (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    best_group_size INT,
+    game_time_in_seconds INT,
+    description VARCHAR(510),
+    image_link VARCHAR(510),
+    bgg_link VARCHAR(510)
+);
