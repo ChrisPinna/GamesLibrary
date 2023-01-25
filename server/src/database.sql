1032918f5f173c51aaf2b359c890854a623cf6c5
christianpinna <christian.a.pinna@gmail.com>
2023-01-25 10:40:58 +0000

    Updated added new column to database

--- server/src/database.sql
+++ server/src/database.sql
@@ -5,5 +5,6 @@ CREATE TABLE games (
     game_time_in_seconds INT,
     description VARCHAR(510),
     image_link VARCHAR(510),
-    bgg_link VARCHAR(510)
+    bgg_link VARCHAR(510),
+    cubicle_in_library VARCHAR(5)
 );
