cbaed5d222dd896f7ddc584e22fe3fd97c4a6d3b
christianpinna <christian.a.pinna@gmail.com>
2023-01-27 14:52:04 +0000

    Update games routs

--- server/src/Routes/games.ts
+++ server/src/Routes/games.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-import { createGame, deleteGame, getAllGames } from '../controllers/games';
+import { createGame, deleteGame, getAllGames, updateGame } from '../controllers/games';
 
 const router = express.Router();
 
 router.get('/',getAllGames);
 router.post('/', createGame);
 router.delete('/:id', deleteGame);
+router.put('/:id', updateGame);
 
 export default router;
