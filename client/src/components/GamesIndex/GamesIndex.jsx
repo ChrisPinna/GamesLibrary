5b8dc9d7492a5916905e63ba30a71287d23f8040
christianpinna <christian.a.pinna@gmail.com>
2023-01-31 15:55:17 +0000

    Updated GameIndex

--- client/src/components/GamesIndex/GamesIndex.jsx
+++ client/src/components/GamesIndex/GamesIndex.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
+import GameDisplay from "../GameDisplay/GameDisplay";
 
 const GamesIndex = () => {
   const [data, setData] = useState(null);
@@ -9,11 +10,30 @@ const GamesIndex = () => {
   useEffect(() => {
     axios
       .get("http://localhost:3000/api/games/")
-      .then((response) => setData(response.data))
+      .then((response) => setData(response.data.message))
       .catch((erros) => console.error(erros));
   }, []);
 
-  return <div>{data ? JSON.stringify(data) : "Loading..."}</div>;
+  const renderGameDisplays = (data) => {
+    return data.map((data) => (
+      <GameDisplay
+        id={data.id}
+        gameName={data.name}
+        description={data.description}
+        imageLink={data.image_link}
+        bestGroupSize={data.best_group_size}
+        playTime={data.game_time_in_seconds / 60}
+        BGGLink={data.bgg_link}
+      />
+    ));
+  };
+
+  return (
+    <div>
+      <div>{data ? renderGameDisplays(data) : "Loading..."}</div>
+      <div>{data ? JSON.stringify(data) : "Loading..."}</div>
+    </div>
+  );
 };
 
 export default GamesIndex;
