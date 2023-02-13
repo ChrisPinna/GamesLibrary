94b883a33de88e8001757f7a3886039bff377d37
christianpinna <christian.a.pinna@gmail.com>
2023-02-13 15:40:31 +0000

    Updated database.sql

--- server/src/database.sql
+++ server/src/database.sql
@@ -1,9 +1,9 @@
 CREATE TABLE games (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
-    best_group_size VARCHAR(15),
-    game_time_in_min VARCHAR(15),
-    description VARCHAR(510),
+    players VARCHAR(15),
+    playtime VARCHAR(15),
+    description VARCHAR(2000),
     image_link VARCHAR(510),
     bgg_link VARCHAR(510),
     cubicle_in_library VARCHAR(5)
