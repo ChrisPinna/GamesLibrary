5452222767ddc474d2af2c3390607b847483970c
christianpinna <christian.a.pinna@gmail.com>
2023-03-10 16:15:59 +0000

    Updated to add class names for CSS and imported CSS

--- client/src/components/GameDisplay/GameDisplay.jsx
+++ client/src/components/GameDisplay/GameDisplay.jsx
@@ -1,19 +1,21 @@
 import axios from "axios";
+import './GameDisplay.css'
 
 const GameDisplay = (props) => {
 
   return (
-    <div>
-      <div>
-        <strong>{props.data.name}</strong>
-      </div>
-      <img src={props.data.image_link} />
-      <div>Players: {props.data.players}</div>
-      <div>Play time: {props.data.playtime}Min</div>
-      <div>Age: {props.data.min_age}+</div>
-      <p>{props.data.description_preview}</p>
-      <div>{props.data.cubicle ? <div>{props.data.cubicle}</div> : " "}</div>
-      <br />
+    <div className="main">
+        <img className="art" src={props.data.image_link} />
+        <div className="name">
+          <strong >{props.data.name}</strong>
+        </div>
+        <div className="details">
+          <div>Players: {props.data.players}</div>
+          <div>Play time: {props.data.playtime}Min</div>
+          <div>Age: {props.data.min_age}+</div>
+          <p>{props.data.description_preview}</p>
+          <div>{props.data.cubicle ? <div>{props.data.cubicle}</div> : " "}</div>
+        </div>
     </div>
   );
 };
