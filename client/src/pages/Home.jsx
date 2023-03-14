74d7dd122d1fb53c20f4ae07b015c2bf80142761
christianpinna <christian.a.pinna@gmail.com>
2023-03-14 12:47:26 +0000

    Added pages directory with home page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/Banner/Banner'
+import GamesIndex from '../components/GamesIndex/GamesIndex'
+
+const Home = () => {
+  return (
+    <div>
+      <Banner/>
+      <GamesIndex/>
+    </div>
+  )
+}
+
+export default Home
