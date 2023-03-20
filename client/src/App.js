06e388aae0e0b54ae945f69e46086073a64029d7
christianpinna <christian.a.pinna@gmail.com>
2023-03-20 11:21:40 +0000

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,16 @@
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AddGame from "./components/AddGame/AddGame";
 import GamesSelection from "./components/BGAGamesSelection/BGAGamesSelection";
-import Home from "./pages/Home";
-
+import Home from "./pages/Home/Home";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/addgame" element={<AddGame/>}></Route>
-        <Route path="/gamesselection" element={<GamesSelection/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/addgame" element={<AddGame />}></Route>
+        <Route path="/gamesselection" element={<GamesSelection />}></Route>
       </Routes>
     </Router>
   );
