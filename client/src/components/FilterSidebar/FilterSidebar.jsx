5266098163e73eaf9994378aee4efcbc0d2be7e9
christianpinna <christian.a.pinna@gmail.com>
2023-03-20 11:24:56 +0000

    Added FilterSidebar component

---
+++ client/src/components/FilterSidebar/FilterSidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './FilterSidebar.css'
+
+const FilterSidebar = () => {
+  return (
+    <div className='filter-sidebar-container'>FilterSidebar</div>
+  )
+}
+
+export default FilterSidebar
