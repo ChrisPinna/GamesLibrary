d3256ef2872e3cb5c948583a8ab16e5fd1a0cea6
christianpinna <christian.a.pinna@gmail.com>
2023-03-23 14:20:10 +0000

    Updated GameDisplay

--- client/src/components/GameDisplay/GameDisplay.jsx
+++ client/src/components/GameDisplay/GameDisplay.jsx
@@ -1,23 +1,26 @@
 import axios from "axios";
-import './GameDisplay.css'
+import "./GameDisplay.css";
+import { Group, AccessAlarm } from '@mui/icons-material';
+
 
 const GameDisplay = (props) => {
-
   return (
-    <div className="main" >
-    <div className="art-container">
-      <img className="art" src={props.data.image_link} />
-    </div>
-        <div className="name">
-          <strong >{props.data.name}</strong>
+    <div className="main">
+      <div className="art-container">
+        <img className="art" src={props.data.image_link} />
+      </div>
+      <div className="name">
+        <strong>{props.data.name}</strong>
+      </div>
+      <div className="details">
+        <div><Group/>{props.data.players}</div>
+        <div>
+           <AccessAlarm/>{props.data.playtime}Min
         </div>
-        <div className="details">
-          <div>Players: {props.data.players}</div>
-          <div>Play time: {props.data.playtime}Min</div>
-          <div>Age: {props.data.min_age}+</div>
-          <p>{props.data.description_preview}</p>
-          <div>{props.data.cubicle ? <div>{props.data.cubicle}</div> : " "}</div>
-        </div>
+        <div>Cubicle: {props.data.cubicle_in_library}</div>
+        <p>{props.data.description_preview}</p>
+        <div>{props.data.cubicle ? <div>{props.data.cubicle}</div> : " "}</div>
+      </div>
     </div>
   );
 };
